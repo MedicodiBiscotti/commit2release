a8ad817bb1545fd958e94b43b81f44b83c01e297
MedicodiBiscotti <13235260+MedicodiBiscotti@users.noreply.github.com>
2024-09-14 00:39:53 +0200

    read instead of awk

--- commit2release.sh
+++ commit2release.sh
@@ -2,14 +2,11 @@
 pattern='^v?\d+(\.\d+){1,}-?[a-z]*'
 
 # Iterate oldest to newest
-git rev-list --reverse --oneline --grep=$pattern -P HEAD | awk '
-{
-    hash = $1;
-    version = $2;
-    tag = "v" version;
-    { printf ("%s: %s\t  ...\tCreating tag: %s\n", hash, version, tag) }
-    system("git tag -f " tag " " hash);
-}'
+git rev-list --reverse --no-commit-header --pretty='format:%H %s' --grep=$pattern -P HEAD | while read -r hash version rest; do
+    tag=v$version
+    printf "%s: %s\t  ...\tCreating tag: %s\n" $hash $version $tag;
+    git tag -f $tag $hash;
+done
 # Force will overwrite tags, i.e. the latest with version in commit message gets the tag.
 
 # When first writing the commits, I got some of the versions wrong. Fix manually at this stage.
