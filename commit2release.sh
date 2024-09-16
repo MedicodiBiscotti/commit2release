4c0dc9157aab1e20ae298fa0c419993aa8290bdb
MedicodiBiscotti <13235260+MedicodiBiscotti@users.noreply.github.com>
2024-09-16 03:00:07 +0200

    adds options for annotated and lightweight tags

--- commit2release.sh
+++ commit2release.sh
@@ -2,8 +2,11 @@
 # Options
 noop=false
 prompt=true
-while getopts n,y opt; do
+lightweight=false
+while getopts l,a,n,y opt; do
     case $opt in
+        a) lightweight=false;;
+        l) lightweight=true;;
         n) noop=true;;
         y) prompt=false;;
     esac
@@ -14,10 +17,24 @@ pattern='^v*\d+(\.\d+){1,}-?[a-z]*'
 # Iterate oldest to newest
 git rev-list --reverse --no-commit-header --pretty='format:%H %s' --grep=$pattern -P HEAD | while read -r hash version rest; do
     tag=$(echo "$version" | sed '1s/^v*/v/')
-    msg=$(git rev-list --no-commit-header --no-walk --pretty='format:%B' $hash | sed '1s/^v*/v/')
+
+    # Still want msg set in dry-runs.
+    if ! $lightweight; then
+        msg=$(git rev-list --no-commit-header --no-walk --pretty='format:%B' $hash | sed '1s/^v*/v/')
+    fi
+
     printf "%s: New tag %s  \t<- %s %s\n" $hash $tag $version "$rest"
+    
     if ! $noop; then
-        git tag -f -m "$msg" $tag $hash
+        # Really annoying that the tag command won't play nice both being lightweight and not.
+        # Subbing in options just doesn't give the right amount of argument in both cases.
+        # Something with the variable expansion and quotes gives too many arguments. Tried many ways, just won't work.
+        # eval works with storing command in variable but is spooky and insecure.
+        if ! $lightweight; then
+            git tag -f -m "$msg" $tag $hash
+        else
+            git tag -f $tag $hash
+        fi
     fi
 done
 # Force will overwrite tags, i.e. the latest with version in commit message gets the tag.
